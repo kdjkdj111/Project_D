3d5e80426219e92b313e40d10eb0611d95bb028f
Your Name <you@example.com>
2025-08-15 18:18:17 +0900

    first_commit
    change Xml

--- app/src/main/java/com/steadyroom/project_d/MainActivity.java
+++ app/src/main/java/com/steadyroom/project_d/MainActivity.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,11 +37,11 @@ import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnSet;
-    private Button btnGacha;
-    private Button btnShop;
-    private Button btnChra;
-    private Button btnBag;
+    private ImageButton btnSet;
+    private ImageButton btnGacha;
+    private ImageButton btnShop;
+    private ImageButton btnChra;
+    private ImageButton btnBag;
     private Button btncheck;
     private Button btnBattle;
     private TextView textPoint;
